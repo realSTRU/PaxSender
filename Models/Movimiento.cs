034df921bf7ad5b52da7392b23977c11407b2654
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-07-18 16:50:50 -0400

    Agregando modelo de movimietos para Entradas y salidad de articulos

---
+++ Models/Movimiento.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+namespace PaxSender
+{
+public class Movimiento
+{
+    [Key]
+    public int OrdenID { get; set; }
+    public int ArticuloID { get; set; }
+    public int? Cantidad { get; set; }
+    public int? cantidad_anterior { get; set; }
+    public int? cantidad_Resultante {get; set;}
+    public string? Razon { get; set; }
+    public DateTime? Fecha { get; set; }
+    public bool Visible { get; set; }
+
+    public Movimiento ()
+    {
+        this.Cantidad =0;
+        this.Visible = true;
+        this.Fecha= DateTime.Now;
+    }
+}
+    public class Entrada : Movimiento
+    {
+        
+    }
+        public class Salida: Movimiento
+    {
+        
+    }
+}
